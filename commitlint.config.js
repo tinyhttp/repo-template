d9bdca410349d776485cde56ce64162618cb9332
v1rtl <hi@v1rtl.site>
2024-10-17 23:47:45 +0300

    fix: biome fmt

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,24 @@
 import _default from '@commitlint/config-conventional'
 
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    ..._default.rules,
-    'type-enum': [
-      2,
-      'always',
-      ['fix', 'test', 'tooling', 'refactor', 'revert', 'example', 'docs', 'format', 'feat', 'chore']
-    ]
-  }
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		..._default.rules,
+		'type-enum': [
+			2,
+			'always',
+			[
+				'fix',
+				'test',
+				'tooling',
+				'refactor',
+				'revert',
+				'example',
+				'docs',
+				'format',
+				'feat',
+				'chore',
+			],
+		],
+	},
 }
